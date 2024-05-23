9ed021adc593b958080532b679e742b5a871f4d6
Inna Polituchaya <innapolituchaya@gmail.com>
2024-05-23 11:53:55 +0400

    Fixed the fib(n)
    Now fib(n) starts the counting with zero, not one

--- mit-project.cpp
+++ mit-project.cpp
@@ -5,7 +5,8 @@ void hello() {
 }
 
 unsigned long long fib(int n) {
-    if (n == 1 || n == 2) return 1; 
+    if (n == 1) return 0; 
+    if (n == 2) return 1; 
     return fib(n - 1) + fib(n - 2); 
 }
 
