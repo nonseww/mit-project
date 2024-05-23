d9ecef01efac58a11a0f50a081863190f0b46291
Inna Polituchaya <innapolituchaya@gmail.com>
2024-05-23 11:51:12 +0400

    Added the function fib(n)
    Function fib(n) gets the number and counts n-number fibonacci

--- mit-project.cpp
+++ mit-project.cpp
@@ -4,8 +4,16 @@ void hello() {
     std::cout << "Hello, World!\n";
 }
 
+unsigned long long fib(int n) {
+    if (n == 1 || n == 2) return 1; 
+    return fib(n - 1) + fib(n - 2); 
+}
+
 int main()
 {
     hello(); 
+    int n; 
+    std::cin >> n; 
+    std::cout << fib(n); 
     return 0; 
 }
